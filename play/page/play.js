3256f9843fa2db6a25a3c9e0e61f26048cac3ea8
k-meltpot <209924027+k-meltpot@users.noreply.github.com>
2025-10-17 19:42:14 +0900

    アニメーションなど

--- play/page/play.js
+++ play/page/play.js
@@ -1,7 +1,7 @@
 class Game {
 	static OP = [
 		{ call: (f, p) => f + p, param: true },
-		{ call: (f, p) => (f - p), param: true },
+		{ call: (f, p) => Math.abs(f - p), param: true },
 		{ call: (f, p) => (f * p), param: true },
 		{ call: (f, p) => (Math.floor(f / p)), param: true },
 		{ call: (f, p) => f % p, param: true },
@@ -27,7 +27,7 @@ class Game {
 			field: -1,
 			op: -1,
 			num: -1
-		}
+		};
 
 		this.input = true;
 	}
@@ -63,7 +63,8 @@ class Game {
 
 		this.onapply(
 			Object.keys(this.value).reduce((acc, key) => ({ ...acc, [key]: this.value[key][this.chosen[key]] }), {}),
-			{ ...this.chosen }
+			{ ...this.chosen },
+			op === 1 && field < num
 		);
 
 		for (const key in this.value) {
@@ -78,15 +79,15 @@ class Game {
 		this.input = false;
 	}
 
-	click(name, index) {
+	click(key, index) {
 		if (!this.input) return;
 
-		if (this.chosen[name] === index) index = -1;
+		if (this.chosen[key] === index) index = -1;
 
-		if (this.chosen[name] !== -1) this.onunfocus(name, this.chosen[name]);
-		if (index !== -1) this.onfocus(name, index);
+		if (this.chosen[key] !== -1) this.onunfocus(key, this.chosen[key]);
+		if (index !== -1) this.onfocus(key, index);
 
-		this.chosen[name] = index;
+		this.chosen[key] = index;
 	}
 
 	get cleared() {
@@ -114,7 +115,18 @@ for (const key in cards) {
 		});
 }
 
-async function applyAnimation(value, index) {
+async function animate(ele, keyframes, duration) {
+	const anime = ele.animate(keyframes, {
+		duration,
+		fill: 'forwards',
+		easing: 'ease-out'
+	});
+	await anime.finished;
+	anime.commitStyles();
+	anime.cancel();
+}
+
+async function applyAnimation(value, index, minus) {
 	game.block();
 
 	const getCenter = (ele) => {
@@ -122,7 +134,7 @@ async function applyAnimation(value, index) {
 		return { x: rect.left + rect.width / 2, y: rect.top + rect.height / 2 };
 	}
 
-	const ele = Object.keys(index).reduce((acc, key) => ({ ...acc, [key]: cards[key][index[key]] }), { body: document.body, apply: cards.apply });
+	const ele = Object.keys(index).reduce((acc, key) => ({ ...acc, [key]: cards[key][index[key]] }), { apply: cards.apply });
 	const center = Object.keys(ele).reduce((acc, key) => ({ ...acc, [key]: getCenter(ele[key]) }), {});
 
 	ele.dummy = cards.dummy;
@@ -132,142 +144,118 @@ async function applyAnimation(value, index) {
 	for (const key in ele)
 		ele[key].style.zIndex = 1;
 
+	// カードを中心に
 	await Promise.all(
-		['field', 'op', 'num', 'apply'].map((name, i) =>
-			ele[name].animate(
-				{
-					translate: `calc(${center.body.x - center[name].x}px + ${20 * (i - 2) - 5}rem) ${center.body.y - center[name].y}px`,
-					scale: (name === 'field' ? 2 / 3 : 1) * 1.5
-				},
-				{
-					duration: 500,
-					fill: 'forwards',
-					easing: 'ease-out'
-				}
-			).finished
-		)
+		['field', 'op', 'num', 'apply'].map((key, i) => {
+			let to = i;
+			if (minus && (key == 'field' || key == 'num')) to = 2 - to;
+			return animate(ele[key], {
+				translate: `calc(50vw - ${center[key].x}px + ${20 * (to - 2) - 5}rem) calc(50vh - ${center[key].y}px)`,
+				scale: (key === 'field' ? 2 / 3 : 1) * 1.5
+			}, 500);
+		})
 	);
 
+	// 両端のカード入れ替え
 	Object.assign(ele.dummy.style, {
 		display: 'flex',
-		left: `calc(${center.body.x}px - 45rem) `,
-		top: `${center.body.y}px`,
+		left: `calc(50vw - ${minus ? 5 : 45}rem) `,
+		top: `50vh`,
 		translate: '-50% -50%',
 		scale: 1.5
 	});
 	ele.dummy.textContent = ele.field.textContent;
 
 	ele.field.textContent = `${value.field}`;
-	ele.field.getAnimations().forEach((anime) => anime.cancel());
-	ele.field.style.translate = `calc(${center.body.x - center.field.x}px + 40rem) ${center.body.y - center.field.y}px`;
+	ele.field.style.translate = `calc(50vw - ${center.field.x}px + 40rem) calc(50vh - ${center.field.y}px)`;
 
-	await ele.field.animate(
-		[
-			{
-				opacity: 0,
-				scale: 2.5
-			},
-			{
-				opacity: 1,
-				scale: 1.8
-			},
-		],
+	// 答えのカード出現
+	await animate(ele.field, [
 		{
-			duration: 500,
-			fill: 'forwards',
-			easing: 'ease-out'
-		}
-	).finished;
-
-	await new Promise((res) => setTimeout(res, 500));
-
-	ele.field.classList.remove('chosen')
-	ele.field.animate(
-		{
-			translate: '0 0',
-			scale: 1
+			opacity: 0,
+			scale: 3
 		},
 		{
-			duration: 500,
-			fill: 'forwards',
-			easing: 'ease-out'
+			opacity: 1,
+			scale: 1.8
+		},
+	], 500);
+
+	await new Promise((res) => setTimeout(res, 1000));
+
+	// 場のカードを戻す
+	ele.field.classList.remove('chosen');
+
+	animate(ele.field, [
+		{
+			scale: 1.8
+		},
+		{
+			translate: value.field === 1
+				? `calc(50vw - ${center.field.x}px + 40rem) calc(-50vh - ${center.field.y}px)`
+				: '0 0',
+			scale: 1
 		}
-	).finished.then(() => {
-		ele.field.getAnimations().forEach((anime) => anime.cancel());
+	], 500).then(() => {
 		ele.field.removeAttribute('style');
+
+		if (value.field === 1) ele.field.style.visibility = 'hidden';
 
 		game.accept();
 	});
 
+	// 手札のカードを消す
 	await Promise.all(
-		['dummy', 'op', 'num', 'apply'].map((name) =>
-			ele[name].animate(
-				{
-					scale: 0,
-					opacity: 0
-				},
-				{
-					duration: 500,
-					fill: 'forwards',
-					easing: 'ease-out'
-				}
-			).finished
+		['dummy', 'op', 'num', 'apply'].map((key) =>
+			animate(ele[key], {
+				scale: 0,
+				opacity: 0
+			}, 300)
 		)
 	);
 
-	ele.dummy.getAnimations().forEach((anime) => anime.cancel());
 	ele.dummy.style.display = 'none';
 
 	displayOperator(index.op, value.op);
 	ele.num.textContent = `${value.num}`;
 
-	ele.op.classList.remove('chosen');
-	ele.num.classList.remove('chosen');
-	ele.apply.classList.remove('chosen');
-
+	// 手札のカードを再出現
 	await Promise.all(
-		['op', 'num', 'apply'].map((name) => {
-			ele[name].getAnimations().forEach((anime) => anime.cancel());
-			return ele[name].animate(
-				[
-					{
-						scale: 0,
-						opacity: 0
-					},
-					{
-						scale: 1,
-						opacity: 1
-					}
-				],
+		['op', 'num', 'apply'].map((key) => {
+			ele[key].style.translate = '0 0';
+			ele[key].classList.remove('chosen');
+			return animate(ele[key], [
 				{
-					duration: 100,
-					fill: 'forwards',
-					easing: 'ease-out'
+					scale: 0,
+					opacity: 0
+				},
+				{
+					scale: 1,
+					opacity: 1
 				}
-			).finished;
+			], 200);
 		})
 	);
 
-	['op', 'num', 'apply'].map((name) => {
-		ele[name].getAnimations().forEach((anime) => anime.cancel());
-		ele[name].removeAttribute('style');
-	})
+	['op', 'num', 'apply', 'dummy'].forEach((key) => {
+		ele[key].removeAttribute('style');
+	});
 }
 
 function displayOperator(index, value) {
-	cards.op[index].textContent = ['+', '-', '*', '/', '%', '&', '|', '^'][value];
+	cards.op[index].textContent = ['+', '-', '×', '÷', '%', '&', '|', '^'][value];
 }
 
 function start() {
 	game = new Game(0);
 
-	game.oninit = (name, index, value) => {
-		if (name === 'op') displayOperator(index, value);
-		else cards[name][index].textContent = `${value}`;
+	game.oninit = (key, index, value) => {
+		if (key === 'op') displayOperator(index, value);
+		else cards[key][index].textContent = `${value}`;
 	};
 
-	game.onfocus = (name, index) => cards[name][index].classList.add('chosen');
-	game.onunfocus = (name, index) => cards[name][index].classList.remove('chosen');
+	game.onfocus = (key, index) => cards[key][index].classList.add('chosen');
+	game.onunfocus = (key, index) => cards[key][index].classList.remove('chosen');
 	game.onapply = applyAnimation;
 
 	game.init();
