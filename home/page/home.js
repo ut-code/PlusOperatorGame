86084fe5f90d4c1109c527c0d0efd90faee763f7
TakumiKojima <ktforuniv@gmail.com>
2025-10-24 11:42:48 +0900

    play.js側でhomeから送られてきた情報を受け取れるように変更

--- home/page/home.js
+++ home/page/home.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('start-button');
     if (startButton) {
         startButton.addEventListener('click', () => {
-            const selectedMode = document.querySelector('input[name="game-mode"]:checked').value;
+            const selectedLevel = document.querySelector('input[name="game-level"]:checked').value;
+            const selectedRule = document.querySelector('input[name="rules"]:checked').value;
             // play.htmlにモード情報を引き継いで画面遷移
-            window.location.href = `../../play/page/play.html?mode=${selectedMode}`;
+            window.location.href = `../../play/page/play.html?level=${selectedLevel}&rule=${selectedRule}`;
         });
     }
 });
