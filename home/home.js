1bdb11806616099859798b7869ed6e8dd7f1ae06
TakumiKojima <ktforuniv@gmail.com>
2025-10-31 11:24:51 +0900

    cpu対戦ページのデザイン

--- home/home.js
+++ home/home.js
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
             const selectedLevel = document.querySelector('input[name="game-level"]:checked').value;
             const selectedRule = document.querySelector('input[name="rules"]:checked').value;
             // play.htmlにモード情報を引き継いで画面遷移
-            window.location.href = `../../solo/play.html?level=${selectedLevel}&rule=${selectedRule}`;
+            if (selectedRule === 'battle') {
+                window.location.href = `../../vsCPU/play.html?level=${selectedLevel}&rule=${selectedRule}`;
+                return;
+            }
+            else {
+                window.location.href = `../../solo/play.html?level=${selectedLevel}&rule=${selectedRule}`;
+            }
         });
     }
 });
