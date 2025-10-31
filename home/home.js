3d3c7a9c39ec8863131d6aadf87b96f959b93179
TakumiKojima <ktforuniv@gmail.com>
2025-10-31 10:51:42 +0900

    フォルダ構成の再修正

--- home/home.js
+++ home/home.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const selectedLevel = document.querySelector('input[name="game-level"]:checked').value;
             const selectedRule = document.querySelector('input[name="rules"]:checked').value;
             // play.htmlにモード情報を引き継いで画面遷移
-            window.location.href = `../../play/page/play.html?level=${selectedLevel}&rule=${selectedRule}`;
+            window.location.href = `../../solo/play.html?level=${selectedLevel}&rule=${selectedRule}`;
         });
     }
 });
